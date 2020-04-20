9d6203c8ae2248b6feb51ae1f6091c384291269a
Aleck Bilounga <hyorickgerrard@gmail.com>
2020-04-20 21:09:51 +0200

    repository de ce que j'ai fait pour la première partie du dm

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,34 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'restaurant',
+        loadChildren: () => import('./restaurant/restaurant.module').then(m => m.JhispterBiloungaaleckRestaurantModule)
+      },
+      {
+        path: 'cooperative',
+        loadChildren: () => import('./cooperative/cooperative.module').then(m => m.JhispterBiloungaaleckCooperativeModule)
+      },
+      {
+        path: 'basket',
+        loadChildren: () => import('./basket/basket.module').then(m => m.JhispterBiloungaaleckBasketModule)
+      },
+      {
+        path: 'product',
+        loadChildren: () => import('./product/product.module').then(m => m.JhispterBiloungaaleckProductModule)
+      },
+      {
+        path: 'payment',
+        loadChildren: () => import('./payment/payment.module').then(m => m.JhispterBiloungaaleckPaymentModule)
+      },
+      {
+        path: 'course',
+        loadChildren: () => import('./course/course.module').then(m => m.JhispterBiloungaaleckCourseModule)
+      },
+      {
+        path: 'order',
+        loadChildren: () => import('./order/order.module').then(m => m.JhispterBiloungaaleckOrderModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
